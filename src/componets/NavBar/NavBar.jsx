5031522df862ca11cef0f9762c01b6bbafa04f60
trawssy <matuserafini@gmail.com>
2024-04-11 19:46:17 -0300

    complete

--- src/componets/NavBar/NavBar.jsx
+++ src/componets/NavBar/NavBar.jsx
@@ -32,7 +32,10 @@ export const NavBar = () => {
             <a href="#contact">Contacto</a>
           </li>
           <li className={style.liNavBarMovile}>
-            <a onClick={openNav}>Abrir menú</a>
+            <a onClick={openNav}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+              </svg></a>
           </li>
         </ul>
       </nav>
@@ -52,7 +55,10 @@ export const NavBar = () => {
               <a href="#contact">Contact</a>
             </li>
             <li className={style.liNavBarMovile} onClick={closeNav} >
-              <a>Cerrar menú</a>
+              <a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                </svg></a>
             </li>
           </div>
         </div>
